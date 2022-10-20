36bb498ac3d174d01852aee7c2d6b2057ff57fba
kostnerek <filip.kostecki00@gmail.com>
2022-10-20 14:05:33 +0200

    feat(user): recreated usermode with sequelize

--- src/app.js
+++ src/app.js
@@ -3,19 +3,23 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 import bodyParser from "body-parser";
 import authRouter from './routes/auth.route.js';
-import connectToDB from './db.js';
+/* import connectToDB from './db.js'; */
+import db from './models/index.js';
 
 dotenv.config({ path: `./.env.${process.env.NODE_ENV}` })
 
 const app = express();
 const port = process.env.PORT || 3000;
-connectToDB();
+/* connectToDB(); */
+db.sequelize.sync();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('<:remote-addr> :remote-user |:method :url - :status| :user-agent :response-time ms [:date[iso]]'));
 
 app.use('/auth', authRouter);
+
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
