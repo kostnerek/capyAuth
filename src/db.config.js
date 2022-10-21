3d20b176a0b15a40d7f0cd15b165303970dbd898
kostnerek <filip.kostecki00@gmail.com>
2022-10-21 12:18:52 +0200

    refactor(db-config): added db dialect and deleted old mongoose code

--- src/db.config.js
+++ src/db.config.js
@@ -1,28 +1,12 @@
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
-
-/**
- *  Function to connect to the mongodb, 
- *  for now it only supports logging in with username and password
- */ 
-const connectToDB = () => {
-    mongoose.connect("mongodb://"+process.env.MONGO_HOST, { 
-        dbName: process.env.MONGO_DB,
-        authSource: process.env.MONGO_AUTH_DB,
-        user: process.env.MONGO_USER,
-        pass: process.env.MONGO_PASSWORD,
-        useNewUrlParser: true, 
-        useUnifiedTopology: true 
-    });
-}
 
 const dbConfig = {
     HOST: process.env.DB_HOST,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASSWORD,
     DB: process.env.DB_NAME,
-    dialect: 'postgres',
+    dialect: process.env.DB_DIALECT,
     pool: {
         max: 5,
         min: 0,
@@ -30,9 +14,5 @@ const dbConfig = {
         idle: 10000
     }
 };
-
-
-
-
 
 export default dbConfig;
